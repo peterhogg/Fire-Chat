44d62222096d700079c11aaa0735c82c9ca1996f
Peter Hoggarth <hoggarth.peter@gmail.com>
2019-02-02 12:18:14 -0500

    Added snapshot test to app

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import renderer from 'react-test-renderer';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+
+describe("Fire Chat", () =>{
+	it('renders without crashing', () => {
+		const div = document.createElement('div');
+		ReactDOM.render(<App />, div);
+		ReactDOM.unmountComponentAtNode(div);
+	});
+	it('has a valid snapshot', () =>{
+		const component = renderer.create(
+			<App />
+		);
+		const tree = component.toJSON();
+		expect(tree).toMatchSnapshot();
+	});
+
+})
+	
