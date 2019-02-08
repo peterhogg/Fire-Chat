4a19ffbae474ed92235ff7bcf87ebc76a023cd02
Peter Hoggarth <hoggarth.peter@gmail.com>
2019-02-07 21:06:22 -0500

    Added header

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,22 @@
-import React, { Component } from 'react';
-import './App.css';
-import Chatbox from './components/chatbox';
-import Chat from './components/chat';
+import React, { Component } from "react";
+import "./App.css";
+import Chatbox from "./components/chatbox";
+import Chat from "./components/chat";
+import Header from "./components/header/index";
+import { HeaderItem } from './models/HeaderItem';
 
 class App extends Component {
-  render() {
-    return (
-      <div>
-		<Chat />
-	  	<Chatbox />
-      </div>
-    );
-  }
+
+    render() {
+
+        return (
+            <div>
+                <Header title="Fire Chat" />
+                <Chat />
+                <Chatbox />
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ src/models/HeaderItem.ts
@@ -0,0 +1,5 @@
+export interface HeaderItem {
+	title: string,
+	url: string,
+	id: string,
+}
